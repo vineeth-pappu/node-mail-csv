9a8f0baaa3e8409d3fb424a91dc9afb18aa06fe3
Vineeth Viswanathan <vineeth0394@gmail.com>
2020-11-12 02:16:43 +0530

    sd

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.use(express.static("src"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const csvFilePath = "https://srv-store6.gofile.io/download/MsOBqf/sample.csv";
-const csv = require("csvtojson");
-csv()
-  .fromFile(csvFilePath)
-  .then((jsonObj) => {
-    console.log("%%%%%%%%%%%%%%%%%%%%%%%%%");
-    console.log(jsonObj);
-    console.log("%%%%%%%%%%%%%%%%%%%%%%%%%");
-  });
+// const csvFilePath = "https://srv-store6.gofile.io/download/MsOBqf/sample.csv";
+// const csv = require("csvtojson");
+// csv()
+//   .fromFile(csvFilePath)
+//   .then((jsonObj) => {
+//     console.log("%%%%%%%%%%%%%%%%%%%%%%%%%");
+//     console.log(jsonObj);
+//     console.log("%%%%%%%%%%%%%%%%%%%%%%%%%");
+//   });
 
 app.post("/send-email", function (req, res) {
   let transporter = nodeMailer.createTransport({
@@ -27,13 +27,13 @@ app.post("/send-email", function (req, res) {
     secure: true,
     auth: {
       // should be replaced with real sender's account
-      user: "vineeth@globeshout.com",
-      pass: "iamlegend0394"
+      user: "gnedufair5@360xglobal.com",
+      pass: "Vr360@1234"
     }
   });
   let mailOptions = {
     // should be replaced with real recipient's account
-    to: "vineeth@globeshout.com",
+    to: "Tania@thinkubatorx.com",
     subject: req.body.subject,
     body: req.body.message
   };
